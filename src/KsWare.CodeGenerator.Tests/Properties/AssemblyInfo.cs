3abd31e8b150bae92d55ec463255bee66dba0171
Appveyor-KsWare <appveyor@ksware.de>
2018-04-05 13:05:58 -0700

    [skip ci] increment version 0.2.13+20180405130503

--- src/KsWare.CodeGenerator.Tests/Properties/AssemblyInfo.cs
+++ src/KsWare.CodeGenerator.Tests/Properties/AssemblyInfo.cs
@@ -15,6 +15,6 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("4875eada-5cc3-46d7-ae0a-8a1a710d15c5")]
 
-// [assembly: AssemblyVersion("0.2.12")]
-[assembly: AssemblyVersion("0.2.12")]
-[assembly: AssemblyFileVersion("0.2.12")]
+// [assembly: AssemblyVersion("0.2.13")]
+[assembly: AssemblyVersion("0.2.13")]
+[assembly: AssemblyFileVersion("0.2.13")]
